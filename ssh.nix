2bdef6697b702cfda6268e3aa381901791ba01f4
Cozy64 <87704025+Cozy64@users.noreply.github.com>
2025-09-22 09:54:47 +0800

    fix: adds service

--- ssh.nix
+++ ssh.nix
@@ -1,15 +1,18 @@
+{pkgs, ...}:
   {
-		openssh = {
-			enable = true;
-			ports = [ 6969 ];
-			settings = {
-				PasswordAuthentication = true;
-				#AllowUsers = null; # Allows all users by default. Can be [ "user1" "user2" ]
-				#UseDns = true;
-				#X11Forwarding = false;
-				PermitRootLogin = "prohibit-password";
-			};
+      services = {
+      openssh = {
+        enable = true;
+        ports = [ 6969 ];
+        settings = {
+          PasswordAuthentication = true;
+          #AllowUsers = null; # Allows all users by default. Can be [ "user1" "user2" ]
+          #UseDns = true;
+          #X11Forwarding = false;
+          PermitRootLogin = "yes";
+        };
 
-		};
+      };
+    };
 
   }
