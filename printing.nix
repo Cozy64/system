67b515c61c48ffa5d4c24ca5d644894bbe55a472
Cozy64 <87704025+Cozy64@users.noreply.github.com>
2025-09-22 10:22:20 +0800

    feat: adds printing

---
+++ printing.nix
@@ -0,0 +1,18 @@
+{pkgs, ...}:
+{
+
+  services = {
+    printing = {
+      enable = true;
+      drivers = with pkgs; [
+        gutenprint
+        hplipWithPlugin
+      ];
+    };
+
+
+  };
+
+
+
+}
