07271d9d146fc84c309e031000c4ffc9e896af36
Cozy64 <87704025+Cozy64@users.noreply.github.com>
2025-09-23 13:14:13 +0800

    feat: merges displaymanager with hyprland

--- gui.nix
+++ gui.nix
@@ -1,5 +1,14 @@
 {pkgs, ...}:
 {
+  programs = {
+    hyprland = {
+      enable = true;
+      #withUWSM = true;
+      #systemd.setPath.enable = true;
+      #package = stable.hyprland;
+      #portalPackage = stable.xdg-desktop-portal-hyprland;
+    };
+  };
   services = {
     
     displayManager = {
