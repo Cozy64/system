fab6e0d77a550dfdafa5a931da66abd21ae23b7d
Cozy64 <87704025+Cozy64@users.noreply.github.com>
2025-12-29 03:13:53 +0800

    adds power-profiles-daemon

--- flake.nix
+++ flake.nix
@@ -63,7 +63,7 @@
         ./modules/swapfile40.nix
         ./modules/alias.nix
         ./modules/ags.nix
-        ./modules/tlp-amd.nix
+        ./modules/power-profiles-daemon.nix
         ./modules/virtualisation.nix
         ./modules/ssh.nix
         ./modules/language.nix
