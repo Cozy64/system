8508f38172719fe6e8c6e08f8b32426793444a19
Cozy64 <87704025+Cozy64@users.noreply.github.com>
2026-01-28 05:53:30 +0800

    ags module additions

--- modules/ags.nix
+++ modules/ags.nix
@@ -15,6 +15,14 @@
       # additional packages and executables to add to gjs's runtime
       extraPackages = with pkgs; [
         inputs.astal.packages.${pkgs.stdenv.hostPlatform.system}.battery
+        inputs.astal.packages.${pkgs.stdenv.hostPlatform.system}.auth
+        inputs.astal.packages.${pkgs.stdenv.hostPlatform.system}.bluetooth
+        inputs.astal.packages.${pkgs.stdenv.hostPlatform.system}.notifd
+        inputs.astal.packages.${pkgs.stdenv.hostPlatform.system}.tray
+        inputs.astal.packages.${pkgs.stdenv.hostPlatform.system}.hyprland
+        inputs.astal.packages.${pkgs.stdenv.hostPlatform.system}.network
+        inputs.astal.packages.${pkgs.stdenv.hostPlatform.system}.mpris
+        inputs.astal.packages.${pkgs.stdenv.hostPlatform.system}.wireplumber
         fzf
       ];
     };
