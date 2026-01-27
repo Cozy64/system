ef2ba7903f4f0d43239a75cb9bd5792c677a6815
Cozy64 <87704025+Cozy64@users.noreply.github.com>
2026-01-28 05:52:42 +0800

    printing additions

--- modules/printing.nix
+++ modules/printing.nix
@@ -2,11 +2,23 @@
 {
 
   services = {
+    avahi = {
+      enable = true;
+      nssmdns4 = true;
+      openFirewall = true;
+      publish = {
+        enable = true;
+        userServices = true;
+      };
+    };
     printing = {
       enable = true;
       drivers = with pkgs; [
         gutenprint
         hplipWithPlugin
+        cnijfilter2
+        samsung-unified-linux-driver
+        splix
       ];
     };
 
