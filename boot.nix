04053c79c1d32b40683f7b3690d099d53b86b471
Cozy64 <87704025+Cozy64@users.noreply.github.com>
2026-01-28 05:53:13 +0800

    changes kernel to latest

--- boot.nix
+++ boot.nix
@@ -3,7 +3,8 @@
           boot = {
             supportedFilesystems = [ "ntfs" ];
   		kernelParams = ["fbcon=font:VGA8x16" "default_hugepagesz=2M" "hugepagesz=1G" "hugepages=4"];
-            kernelPackages = pkgs.linuxPackages_zen;
+            # kernelPackages = pkgs.linuxPackages_zen;
+            kernelPackages = pkgs.linuxPackages_latest;
             loader = {
               efi.canTouchEfiVariables = true;
               systemd-boot.enable = true;
--- lanzaboote.nix
+++ lanzaboote.nix
@@ -4,7 +4,8 @@
             initrd.systemd.enable = true;
             supportedFilesystems = [ "ntfs" ];
   		kernelParams = ["fbcon=font:VGA8x16" "default_hugepagesz=2M" "hugepagesz=1G" "hugepages=4"];
-            kernelPackages = pkgs.linuxPackages_zen;
+            # kernelPackages = pkgs.linuxPackages_zen;
+            kernelPackages = pkgs.linuxPackages_latest;
           lanzaboote = {
             enable = true;
             pkiBundle = "/var/lib/sbctl";
