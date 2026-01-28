dbd53c96abca8e1b9c0a382b9c2a2d6169ed52cc
Cozy64 <87704025+Cozy64@users.noreply.github.com>
2026-01-28 14:55:07 +0800

    fixes pathing

--- lolminer/grin.sh
+++ lolminer/grin.sh
@@ -1,1 +1,1 @@
-lolminer --coin GRIN-C32 -p asia-grin.2miners.com:3030 -u grin186vgeadlf6hjyz38cka2p4yyw5qle0t4nqqlaktq4u8w6qzjp8xq8zjrts
+./lolMiner --coin GRIN-C32 -p asia-grin.2miners.com:3030 -u grin186vgeadlf6hjyz38cka2p4yyw5qle0t4nqqlaktq4u8w6qzjp8xq8zjrts
